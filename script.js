6903e5172b86bafaf6776d9f905eff68642b61e1
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-23 19:24:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length < 2) {
+        return -Infinity;
+    }
+
+    let firstHighest = -Infinity;
+    let secondHighest = -Infinity;
+
+    for (let num of arr) {
+        if (num > firstHighest) {
+            secondHighest = firstHighest;
+            firstHighest = num;
+        } else if (num > secondHighest && num < firstHighest) {
+            secondHighest = num;
+        }
+    }
+
+    if (secondHighest === -Infinity) {
+        return -Infinity;
+    }
+
+    return secondHighest;
+}
+
+
+// Test cases
+console.log(secondHighest([5, 1, 2, 3, 4]));  // Output: 4
+console.log(secondHighest([-1, -2, -3, -4, -5]));  // Output: -2
+console.log(secondHighest([]));  // Output: -Infinity
+console.log(secondHighest([1]));  // Output: -Infinity
+console.log(secondHighest([1, 1, 1, 1, 1]));  // Output: -Infinity
